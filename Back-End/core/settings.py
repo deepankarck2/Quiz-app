9b83e0d12ee6c4cad2713a48e7e1790807bff994
Deepankar <dip2leo@gmail.com>
2022-12-01 12:50:49 -0500

    l

--- Back-End/core/settings.py
+++ Back-End/core/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
 
     'quiz',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,7 +54,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
