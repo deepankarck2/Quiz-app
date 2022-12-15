20a573befc148f39af6b3f8ed6747d3303caefa9
wshi01 <jayceengineer2@gmail.com>
2022-12-15 15:35:47 -0500

    minor changes

--- front-end/src/components/AllCards.js
+++ front-end/src/components/AllCards.js
@@ -6,14 +6,15 @@ export default function AllCards(){
     const [front, setFront] = useState([]);
     const [back, setBack] = useState([]);
     const [current, setCurrent] = useState(0);
-    const front_items = ['AI', 'Floating point', 'Linear Regression']
+    const front_items = ['AI', 'Floating point', 'Linear Regression', 'You have reached the end of your cards']
     const back_items = [
     "AI (short for artificial intelligence) is the simulation of human intelligence in machines that are programmed to think and act like humans. AI is a broad field that encompasses a wide range of technologies and techniques, including machine learning, natural language processing, and robotics. The goal of AI is to create machines that can perform tasks that typically require human intelligence, such as problem-solving, decision-making, and learning.",
     "A floating point number is a type of numerical representation that is used to store and manipulate decimal values in computer programs. In contrast to fixed point numbers, which have a fixed number of decimal places, floating point numbers use a dynamic decimal point that can move to represent a wider range of values.",
     "Linear regression is a statistical method that is used to model the relationship between a dependent variable and one or more independent variables. In a linear regression model, the dependent variable is assumed to be linearly related to the independent variables, and the model estimates the parameters of the linear relationship using a set of data.",
     ]
     function handleClick() {
-        setCurrent(prevIndex => prevIndex + 1);
+        {back_items.length <= current ? setCurrent(prevIndex => prevIndex - 1) : 
+        setCurrent(prevIndex => prevIndex + 1); }
     }
     function handleClick2() {
         setCurrent(prevIndex => prevIndex - 1);
